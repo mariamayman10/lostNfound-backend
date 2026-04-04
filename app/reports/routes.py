448f60adedd10671fd4a28fdf12804c3c4e4faad
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-04-04 15:42:05 +0200

    add report creation route

--- app/reports/routes.py
+++ app/reports/routes.py
@@ -7,3 +7,40 @@ reports_routes = Blueprint('reports', __name__, url_prefix='/reports')
 report_schema = ReportSchema()
 create_report_schema = CreateReportSchema()
 update_report_schema = UpdateReportSchema()
+@reports_routes.route('/', methods=['GET'])
+def get_reports():
+@reports_routes.route('/create', methods=['POST'])
+@require_auth
+def create_report():
+    try:
+        data = request.form.to_dict()
+        resulted_data = create_report_schema.load(data)
+        user_id = request.user["uid"] # type: ignore
+        images = request.files.getlist('images')
+        result = create_report_service(resulted_data, images, user_id)
+        return jsonify(result), 201
+    except ValidationError as e:
+        return jsonify({
+            "errorMsg": "Validation error",
+            "errors": e.messages
+        }), 400
+    except ValueError as e:
+        return jsonify({"errorMsg": str(e)}), 401
+    except Exception as e:
+        return jsonify({"errorMsg": str(e)}), 400
+
+
+@reports_routes.route('/<id>', methods=['PUT'])
+@require_auth
+def update_report(id):
+
+@reports_routes.route('/<id>', methods=['GET'])
+@require_auth
+def get_report(id):
+
+@reports_routes.route("/my")
+@require_auth
+def get_my_reports():
+
+@reports_routes.route("/report-img/<filename>")
+def server_report_img(filename):
--- app/reports/service.py
+++ app/reports/service.py
@@ -1,5 +1,54 @@
 import datetime, os
+from app import Config
+from app.extensions import get_db
+from app.globalHelpers import save_image, move_file
 from .schema import ReportSchema, GetReportSchema
+from google.cloud.firestore_v1 import Increment
 
 report_schema = ReportSchema()
 get_report_schema = GetReportSchema()
+
+
+def save_report(report, user_id):
+    report = {
+        "title": report['title'],
+        "description": report['description'],
+        "location": report['location'],
+        "category": report['category'],
+        "status": "open",
+        "type": report['type'],
+        "imageUrls": [],
+        "userId": user_id,
+        "createdAt": datetime.datetime.now()
+    }
+    doc_ref = get_db().collection('reports').document()
+    doc_ref.set(report)
+    report_id = doc_ref.id
+    return report, report_id
+
+def create_report_service(report, images, user_id):
+    if len(images) < 1 or len(images) > 3:
+        raise ValueError("You must upload between 1 and 3 images")
+    temp_filepaths = []
+    try:
+        for image in images:
+            temp_filepaths.append(save_image(image, Config.TEMP_UPLOAD_FOLDER))
+
+        firebase_report, report_id = save_report(report, user_id)
+        image_urls = []
+        for temp_filepath in temp_filepaths:
+            image_urls.append(move_file(temp_filepath, Config.REPORTS_UPLOAD_FOLDER))
+        get_db().collection('reports').document(report_id).update({
+            "imageUrls": image_urls
+        })
+        get_db().collection("users").document(user_id).update({
+            "postsCount": Increment(1)
+        })
+        firebase_report['imageUrls'] = image_urls
+        return report_schema.dump(firebase_report)
+
+    except Exception as e:
+        for temp_filepath in temp_filepaths:
+            if temp_filepaths and os.path.exists(temp_filepath):
+                os.remove(temp_filepath)
+        raise e
