540fa4fc2a587d8820090f70a8df4342a233bfa6
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-04-04 15:47:38 +0200

    implement get user by id

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,2 +1,18 @@
 from flask import Blueprint, send_from_directory, jsonify
+from app.config import Config
+from .service import get_user_by_id
+import os
 users_routes = Blueprint('users', __name__, url_prefix='/users')
+@users_routes.route("/profile/<filename>")
+def serve_profile_pic(filename):
+    os.path.join(Config.PROFILE_UPLOAD_FOLDER, filename)
+    return send_from_directory(Config.PROFILE_UPLOAD_FOLDER, filename)
+
+@users_routes.route("/<id>")
+def get_user(id):
+    try:
+        user = get_user_by_id(id)
+        print(user)
+        return jsonify(user), 200
+    except ValueError as e:
+        return jsonify({'errorMsg': str(e)}), 401
--- app/users/service.py
+++ app/users/service.py
@@ -2,3 +2,10 @@ from app.extensions import get_db
 from .schema import UserSchema
 
 user_schema = UserSchema()
+
+def get_user_by_id(user_id):
+    doc = get_db().collection('users').document(user_id).get()
+    if not doc.exists:
+        return "User not found"
+    user = doc.to_dict()
+    return user_schema.dump(user)
