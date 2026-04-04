e4bedce26c35637f5ad7e0639e84fdd5f4f802e1
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-04-04 15:48:00 +0200

    implement comment/reply creation

--- app/comments/routes.py
+++ app/comments/routes.py
@@ -1,6 +1,23 @@
 from flask import Blueprint, request, jsonify
 from marshmallow import ValidationError
+
+from app.auth.decorators import require_auth
 from .schema import CommentSchema
+from .service import create_comment_service
 
 comments_routes = Blueprint('comments', __name__)
 comments_schema = CommentSchema()
+
+@comments_routes.route('/comments', methods=['POST'])
+@require_auth
+def create_comment():
+    try:
+        user_id = request.user["uid"]  # type: ignore
+        comment_data = comments_schema.load(request.json)
+        comment = create_comment_service(comment_data, user_id)
+        return jsonify(comment), 201
+    except ValueError:
+        return jsonify({'errorMsg': 'Invalid JSON format'}), 400
+    except ValidationError:
+        return jsonify({'errorMsg': 'Invalid JSON format'}), 400
+
--- app/comments/service.py
+++ app/comments/service.py
@@ -5,3 +5,45 @@ from app.extensions import get_db
 
 comments_schema = CommentSchema()
 
+
+def create_comment_service(comment_data, user_id):
+    report = get_db().collection("reports").document(comment_data["reportId"]).get()
+    if not report.exists:
+        raise ValueError("Report not found")
+    if comment_data["parentId"]:
+        parent = get_db().collection("comments").document(comment_data["parentId"]).get()
+        if not parent.exists:
+            raise ValueError("Parent comment not found")
+
+        parent_data = parent.to_dict()
+        if parent_data["reportId"] != comment_data["reportId"]:
+            raise ValueError("Invalid parent comment")
+        if parent_data.get("parentId"):
+            raise ValueError("Cannot reply to a reply")
+
+    doc_ref = get_db().collection("comments").document()
+    comment = {
+        "userId": user_id,
+        "content": comment_data["content"],
+        "reportId": comment_data["reportId"],
+        "parentId": comment_data["parentId"],
+        "createdAt": datetime.datetime.now(),
+    }
+    doc_ref.set(comment)
+    user = get_db().collection("users").document(user_id).get()
+    user_data = user.to_dict()
+    comment["userName"] = user_data["name"]
+    return comments_schema.dump(comment)
+
+def get_report_comments(report_id):
+    docs = get_db().collection("comments").where("reportId", "==", report_id).stream()
+
+    comments = []
+    for doc in docs:
+        data = doc.to_dict()
+        user = get_db().collection("users").document(data["userId"]).get()
+        user_data = user.to_dict()
+        data["id"] = doc.id
+        data["userName"] = user_data["name"]
+        comments.append(data)
+    return comments
