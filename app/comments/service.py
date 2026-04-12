a164bbc7298d71adafb0f1bc2318338a9fcf0938
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2026-04-12 13:33:41 +0200

    push notifications after creating comment/reply

--- app/comments/service.py
+++ app/comments/service.py
@@ -1,27 +1,75 @@
 import datetime
-
+from string import Template
 from app.comments.schema import CommentSchema
 from app.extensions import get_db
 
 comments_schema = CommentSchema()
 
+reply_str = "$name replied to your comment on $title report"
+reply_obj = Template(reply_str)
+comment_str = "$name commented on your $title report"
+comment_obj = Template(comment_str)
+
+def create_notifications(user_data, comment_data, report_data, parent_data, user_id):
+    db = get_db()
+    if parent_data and parent_data["userId"] != user_id:
+        db.collection("users") \
+          .document(parent_data["userId"]) \
+          .collection("notifications") \
+          .add({
+              "message": reply_obj.substitute(
+                  name=user_data["name"],
+                  title=report_data["title"]
+              ),
+              "userId": parent_data["userId"],
+              "seen": False,
+              "created_at": datetime.datetime.now(),
+              "type": "reply",
+              "reportId": comment_data["reportId"]
+          })
+
+    elif report_data["userId"] != user_id:
+        db.collection("users") \
+          .document(report_data["userId"]) \
+          .collection("notifications") \
+          .add({
+              "message": comment_obj.substitute(
+                  name=user_data["name"],
+                  title=report_data["title"]
+              ),
+              "userId": report_data["userId"],
+              "seen": False,
+              "created_at": datetime.datetime.now(),
+              "type": "comment",
+              "reportId": comment_data["reportId"]
+          })
+
 
 def create_comment_service(comment_data, user_id):
-    report = get_db().collection("reports").document(comment_data["reportId"]).get()
+    db = get_db()
+    report = db.collection("reports").document(comment_data["reportId"]).get()
     if not report.exists:
         raise ValueError("Report not found")
+    report_data = report.to_dict()
+    if report_data["status"] == "closed":
+        raise ValueError("Report closed")
+
+    parent_data = None
     if comment_data["parentId"]:
-        parent = get_db().collection("comments").document(comment_data["parentId"]).get()
+        parent = db.collection("comments").document(comment_data["parentId"]).get()
+
         if not parent.exists:
             raise ValueError("Parent comment not found")
 
         parent_data = parent.to_dict()
+
         if parent_data["reportId"] != comment_data["reportId"]:
             raise ValueError("Invalid parent comment")
+
         if parent_data.get("parentId"):
             raise ValueError("Cannot reply to a reply")
 
-    doc_ref = get_db().collection("comments").document()
+    doc_ref = db.collection("comments").document()
     comment = {
         "userId": user_id,
         "content": comment_data["content"],
@@ -30,9 +78,12 @@ def create_comment_service(comment_data, user_id):
         "createdAt": datetime.datetime.now(),
     }
     doc_ref.set(comment)
-    user = get_db().collection("users").document(user_id).get()
+
+    user = db.collection("users").document(user_id).get()
     user_data = user.to_dict()
     comment["userName"] = user_data["name"]
+
+    create_notifications(user_data, comment_data, report_data, parent_data, user_id)
     return comments_schema.dump(comment)
 
 def get_report_comments(report_id):
